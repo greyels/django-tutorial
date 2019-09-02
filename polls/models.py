4653224ff5d8be6d3bc95085bd611084478a671c
Sergey Grishin <sgrishin@admins-Mac-mini-4.local>
2019-09-02 18:05:10 +0300

    Admin part of the tutorial

--- polls/models.py
+++ polls/models.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 class Question(models.Model):
     """docstring for Question"""
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date_published')
+    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.question_text
@@ -17,6 +17,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
